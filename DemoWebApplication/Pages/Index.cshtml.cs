f04463be1edcb57a41e6bd42783f9048420bfd6a
Michal Altair Valášek <michal.valasek@altairis.cz>
2019-08-28 21:20:52 +0200

    Added default web app project for demo

---
+++ DemoWebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoWebApplication.Pages {
+    public class IndexModel : PageModel {
+        public void OnGet() {
+
+        }
+    }
+}
