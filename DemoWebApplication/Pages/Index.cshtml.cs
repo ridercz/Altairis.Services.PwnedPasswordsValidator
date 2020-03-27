249e0c26ffed3d2bb0d728a836de40a6a06ce985
Michal Altair Valášek <michal.valasek@altairis.cz>
2020-03-27 15:55:39 +0100

    Deleted 2.2 demo application

--- DemoWebApplication/Pages/Index.cshtml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace DemoWebApplication.Pages {
-    public class IndexModel : PageModel {
-        public void OnGet() {
-
-        }
-    }
-}
--- DemoWebApplication/Pages/Privacy.cshtml.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace DemoWebApplication.Pages {
-    public class PrivacyModel : PageModel {
-        public void OnGet() {
-        }
-    }
-}
